47c2f9bd00de0ef4b2054cbe02bd1d62d5c8e65a
Dmytro <@email.com>
2026-08-12 05:00:11 +0300

    lol4

--- app/main.py
+++ app/main.py
@@ -1,11 +1,20 @@
 def copy_file(command: str) -> None:
     parts = command.split()
+
+    if len(parts) != 3:
+        return
+
+    if parts[0] != "cp":
+        return
+
+    if parts[1] == parts[2]:
+        return
+
     try:
-        if not parts[0] == "cp":
-            return
-        if not parts[1] == parts[2]:
-            with (open(parts[1], "r") as source_file,
-                  open(parts[2], "w") as destination_file):
-                destination_file.write(source_file.read())
-    except Exception as e:
-        print(e)
+        with (
+            open(parts[1], "r") as source_file,
+            open(parts[2], "w") as destination_file
+        ):
+            destination_file.write(source_file.read())
+    except FileNotFoundError:
+        return
