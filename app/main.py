cd1488cee1d226750ad374d8dae4ccccae8e177b
Dmytro <@email.com>
2026-08-12 04:48:05 +0300

    LOL

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(copy: str) -> None:
+    lst = copy.split()
+    try:
+        if not lst[0] == "cp":
+            return
+        if not lst[1] == lst[2]:
+            with open(lst[1], "r") as f, open(lst[2], "w") as g:
+                g.write(f.read())
+    except Exception as e:
+        print(e)
